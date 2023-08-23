0ae99788ac762a3d8cf0058e2e96c637af25ee42
Peter2029 <90644480+Peter2029@users.noreply.github.com>
2023-08-23 13:06:22 +0200

    eger

--- app/src/main/java/com/example/overenie_ecv/SignUpActivity.java
+++ app/src/main/java/com/example/overenie_ecv/SignUpActivity.java
@@ -1,15 +1,70 @@
 package com.example.overenie_ecv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText signUpEmail, signUpPassword;
+    private Button signUpButton;
+    private TextView loginRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        auth = FirebaseAuth.getInstance();
+        signUpEmail = findViewById(R.id.sign_up_email);
+        signUpPassword = findViewById(R.id.sign_up_password);
+        signUpButton = findViewById(R.id.sign_up_btn);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = signUpEmail.getText().toString().trim();
+                String password = signUpPassword.getText().toString().trim();
+                if (user.isEmpty()){
+                    signUpEmail.setError("Vyplňte svoj Email");
+                }
+                if (password.isEmpty()){
+                    signUpPassword.setError("Zadajte heslo");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this, "Registrácia prebehla úspešne", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                            }
+                            else {
+                                Toast.makeText(SignUpActivity.this, "Registrácia bola neúspešná" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
